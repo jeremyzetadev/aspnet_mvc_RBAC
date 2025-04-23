d1f002b9bb9658cd12927a69b8a24a7f86797d0d
jeremy zeta <jeremy_zeta@yahoo.com>
2025-04-23 20:54:58 +0900

    first commit

---
+++ aspnet_mvc-role/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnet_mvc_role.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
